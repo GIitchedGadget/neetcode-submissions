00d6edaa61053386e429bc1bc3c41c3c279d4df6
GIitchedGadget <90413092+GIitchedGadget@users.noreply.github.com>
2026-07-14 21:05:01 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = []
+        groups = {}
+
+        for i in strs:
+            cleaned = "".join(sorted(i))
+            groups[cleaned] = groups.get(cleaned, []) + [i]
+
+        for i in groups:
+            result.append(groups[i])
+            
+        return result
+
+        
+
+        
