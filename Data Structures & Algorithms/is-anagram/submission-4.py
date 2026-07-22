ba496714cc111c41728e9d9edb25f77a5c231fc8
GIitchedGadget <90413092+GIitchedGadget@users.noreply.github.com>
2026-07-22 15:58:02 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = {}
+        if len(s) != len(t):
+            return False
+        
+        for i in s:
+            count[i] = count.get(i, 0) + 1
+        
+        for i in t:
+            if count.get(i, 0) == 0:
+                return False
+            count[i] = count.get(i, 0) - 1
+
+        print(count)
+        return True
+        
